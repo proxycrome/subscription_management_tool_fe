294b2a22d989774be7a744fa3d6cb76c5fbf03c7
proxycrome <78900687+proxycrome@users.noreply.github.com>
2021-11-02 18:45:49 +0100

    blog page

--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -1,12 +1,37 @@
-import React, { useEffect } from 'react'
-import Navbar from '../../Components/Navbar/Navbar'
-import Footer from '../../Components/Footer/Footer'
-import '../Blog/Blog.css'
+import React, { useEffect } from 'react';
+import Navbar from '../../Components/Navbar/Navbar';
+import Footer from '../../Components/Footer/Footer';
+import BlogCard from './BlogCard';
+import data from './BlogData'
+import '../Blog/Blog.css';
+
 function Blog(){
     return(
-        <div className="Blog">
-
-        </div>
+        <>
+            <section>
+                <Navbar/>
+                <div className="Blog">
+                    <div className="heading">
+                        <h1>The news that surrounds us is collected here</h1>
+                        <p>Read, watch and absorb the content that we publish in our news blog. 
+                            Also leave comments and communicate with other users.</p>
+                    </div>
+                    <div className="blogs">
+                        {data
+                            .map((data, index) => (
+                                <BlogCard
+                                    key={index}
+                                    header={data.header}
+                                    body={data.body}
+                                    authorName={data.authorName}
+                                    date={data.date}
+                                />
+                            ))}
+                    </div>
+                </div>
+                <Footer/>
+            </section>
+        </>
     )
 
 }
---
+++ src/Pages/Blog/BlogData.js
@@ -0,0 +1,52 @@
+const BlogData = [
+    {
+        header: 'Omdia foreCasts cloud gaming to nudge $12bn by 2026',
+        body: 'Subscription services such as Xbox Game Pass Ultimate and playStation Now will be the primary force driving cloud gaming helping to generate $3.7bn in total in 2021.',
+        authorName: 'Tina Lawal',
+        date: '02 Dec'
+    },
+    {
+        header: 'Runaway Saas spend...',
+        body: 'Just the management and insight alone got us a long way as we step into...',
+        authorName: 'William Okafor',
+        date: '02 Dec'
+    },
+    {
+        header: 'Growth of SaaS Industry',
+        body: 'Subscription-based Saas application and licensing makes utilizing...',
+        authorName: 'Tina Lawal',
+        date: '02 Dec'
+    },
+    {
+        header: 'Subscription model',
+        body: 'The subscription business model is a business model in which a customer...',
+        authorName: 'Helen Afam',
+        date: '02 Dec'
+    },
+    {
+        header: 'Future of Subscription',
+        body: 'A lot of people don\'t know the future of subscription businesses today...',
+        authorName: 'Helen Afam',
+        date: '02 Dec'
+    },
+    {
+        header: 'Prime video channels',
+        body: 'Prime Video Chennels is launching with 8 different OTT services...',
+        authorName: 'William Okafor',
+        date: '02 Dec'
+    },
+    {
+        header: 'Future of Learning',
+        body: 'A constant ability to learn will be on the most crucial skills...',
+        authorName: 'Helen Afam',
+        date: '02 Dec'
+    },
+    {
+        header: 'Future of Work',
+        body: 'Majority of people will work in jobs that don\'t exist today...',
+        authorName: 'Helen Afam',
+        date: '02 Dec'
+    },
+]
+
+export default BlogData;
