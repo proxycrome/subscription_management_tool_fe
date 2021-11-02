d791f3b7fad91b887d0753cbd81b19c8e0eef3bd
proxycrome <78900687+proxycrome@users.noreply.github.com>
2021-11-02 18:55:16 +0100

    blog page

--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import Navbar from '../../Components/Navbar/Navbar';
 import Footer from '../../Components/Footer/Footer';
 import BlogCard from './BlogCard';
