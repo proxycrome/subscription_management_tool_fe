e64ea9bea1d7d0e3449d864d0c170c2da3ee9579
proxycrome <78900687+proxycrome@users.noreply.github.com>
2021-12-01 21:16:52 +0100

    updates

--- src/Pages/Sign-in/Signin.js
+++ src/Pages/Sign-in/Signin.js
@@ -212,49 +212,9 @@ import '../Sign-in/Signin.css'
             </div>
             </div>
         </div>
-        <div className="mobilesignin">
-            <div className="mobilesigninHeader">
-            <img src={flexpng} style= {{width: "78px",
-        height: "32px"}}/>
-
-        <h3>Stay connected always</h3>
-        </div>
-        <div className="signinmobile-inner">
-            <h2>WELCOME BACK!</h2>
-            <h3>Enter your details to continue...</h3>
-            <div className="signininputWrapper">
-            <input type="email" placeholder="Email address"/>
-            </div>
-            <div className="signininputWrapper" id="password-mobile-signin">
-            <input type={passwordshow ? "text": "password"} name="password" placeholder="Password" value={password}
-            onChange={(e)=>{signindetails({[e.target.name]:e.target.value});setPassword(e.target.value)}}/>
-            <i onClick={handletoggle}><FaEye style={passwordshow ? {display:"none"}:{display:"inline"}}/><FaEyeSlash style={passwordshow ? {display:"inline"}: {display:"none"}}/></i>
-          
-            </div>
-            <div className="forgot-and-remember">
-            
-            <span  className="keep-logged-in"> 
-            <input id="logedIn" type="checkbox" value="loggedin"
-            onChange={(()=>{setLoggedins(!loggedins )})   }/>
-                 <label for="logedIn">Remember me</label>
-            </span>
-            <Link className="forgot"><p className="forgot">Forgot password?</p></Link>
-            </div>
-            <button className="create" onClick={handlesignin}>LOG IN</button>
-          <div className="orandline">  <hr className="line-left"/><p className="loginWith">Log in with</p> <hr className="line-right"/></div>
-          <div className="alternateSignin">
-            <button className="google"><GoogleLogo className="googleLogo" />GOOGLE</button>
-            <button className="facebook"><FacebookLogo className="faceLogo"/>FACEBOOK</button>
-            </div>
- 
-            <p className="noAccount">Don't have an account? <Link to="/signup" className="noAccount-signup">Sign up</Link></p>
-           
-             
-        </div>
         
-         </div>
-      
-       </div>
+ 
+    </div>  
     )
 }
 
